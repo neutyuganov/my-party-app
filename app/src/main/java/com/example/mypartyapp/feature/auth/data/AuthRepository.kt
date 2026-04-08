6952e7fa300fef96012ea38c16bca47d3cd92753
Sergey Utyuganov <utyuganovsergey55@gmail.com>
2026-04-08 21:31:25 +0600

    feat(auth): добавлен AuthRepository

---
+++ app/src/main/java/com/example/mypartyapp/feature/auth/data/AuthRepository.kt
@@ -0,0 +1,26 @@
+package com.example.mypartyapp.feature.auth.data
+
+import com.example.mypartyapp.core.network.supabaseClient
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.providers.builtin.Email
+
+class AuthRepository {
+
+    suspend fun signUp(email: String, password: String) {
+        supabaseClient.auth.signUpWith(Email) {
+            this.email = email
+            this.password = password
+        }
+    }
+
+    suspend fun signIn(email: String, password: String) {
+        supabaseClient.auth.signInWith(Email) {
+            this.email = email
+            this.password = password
+        }
+    }
+
+    suspend fun signOut() {
+        supabaseClient.auth.signOut()
+    }
+}
